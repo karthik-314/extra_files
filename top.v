c783da3bf565f5aec6b4a310cae767e28d06887a
Karthik R Prakash <gestureg@gmail.com>
2025-09-04 21:59:43 +0530

    Create top.v

---
+++ top.v
@@ -0,0 +1,11 @@
+module top(a, b, clk, select, out);
+	input a, b, clk, select;
+	output out;
+	reg out;
+	wire y;
+	assign y=(select) ? b:a;
+	always @(posedge clk)
+	begin
+		out<=y;
+	end
+endmodule
