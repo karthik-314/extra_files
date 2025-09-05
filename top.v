6fea62c15dc7023b0c2852f0d6a89c33476ff7c2
Karthik R Prakash <gestureg@gmail.com>
2025-09-05 09:07:07 +0530

    Update top.v

--- top.v
+++ top.v
@@ -1,11 +1,14 @@
-module top(a, b, clk, select, out);
-	input a, b, clk, select;
-	output out;
-	reg out;
-	wire y;
-	assign y=(select) ? b:a;
-	always @(posedge clk)
-	begin
-		out<=y;
+module top(clk,a, b, x, y, sel, z);
+	input clk;
+	input [7:0] a,b,x,y;
+	input sel;
+	output reg [15:0] z;
+
+	always @(posedge clk) begin
+		if (sel == 1'b0)
+			z = a*b;
+		else
+			z = x*y;
 	end
+
 endmodule
